1dc8379e717d1225b54984fe6b4539c05ab88932
deathworship <125945145+deathworship@users.noreply.github.com>
2023-08-22 02:32:20 +0300

    unicafe step5

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,5 +1,11 @@
 import { useState } from 'react'
 
+const StatisticLine = (props) => {
+  return (
+    <p>{props.text} {props.value} {props.unit}</p>
+  )
+}
+
 const Statistics = (props) => {
   const {good, neutral, bad} = props
   const all = good + neutral + bad
@@ -16,13 +22,19 @@ const Statistics = (props) => {
   return (
     <>
       <h1>statistics</h1>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {all}</p>
-      <p>average {(good*1 + neutral*0 + bad*-1)/all}</p>
-      <p>positive {(good/all)*100} %</p>
+      <StatisticLine text="good" value={good} />
+      <StatisticLine text="neutral" value={neutral} />
+      <StatisticLine text="bad" value={bad} />
+      <StatisticLine text="all" value={all} />
+      <StatisticLine text="average" value={(good*1 + neutral*0 + bad*-1)/all} />
+      <StatisticLine text="positive" value={(good/all)*100} unit="%"/>
     </>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
   )
 }
 
@@ -35,9 +47,9 @@ const App = () => {
   return (
     <div>
       <h1>give feedback</h1>
-      <button onClick={() => setGood(good + 1)}>good</button>
-      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   )
