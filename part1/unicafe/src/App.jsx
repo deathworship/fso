6d35508695a2f807a11d3535a8df5f26f7fab2cd
deathworship <125945145+deathworship@users.noreply.github.com>
2023-08-22 01:59:38 +0300

    unicafe step2

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -5,6 +5,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  let all = good + neutral + bad
 
   return (
     <div>
@@ -16,6 +17,9 @@ const App = () => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {all}</p>
+      <p>average {(good*1 + neutral*0 + bad*-1)/all}</p>
+      <p>positive {(good/all)*100} %</p>
     </div>
   )
 }
