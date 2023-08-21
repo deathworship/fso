140c647a905bfd68dcf3b24ed6ac65f45ef42102
deathworship <125945145+deathworship@users.noreply.github.com>
2023-08-22 02:11:42 +0300

    unicafe step4

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -4,6 +4,15 @@ const Statistics = (props) => {
   const {good, neutral, bad} = props
   const all = good + neutral + bad
 
+  if (all === 0) {
+    return (
+      <>
+        <h1>statistics</h1>
+        <p>No feedback given</p>
+      </>
+    )
+  }
+
   return (
     <>
       <h1>statistics</h1>
