adc7ee5289b6633c02afacdde844f488a4389670
deathworship <125945145+deathworship@users.noreply.github.com>
2023-08-24 00:13:51 +0300

    part2 courseinfo separate module

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -1,41 +1,4 @@
-const Header = (props) => {
-  return (
-    <h2>{props.course.name}</h2>
-  )
-}
-
-const Part = (props) => {
-  return (
-    <p>{props.part} {props.exercises}</p>
-  )
-}
-
-const Content = (props) => {
-  const { parts } = props.course
-  return (
-    <div>
-      {parts.map(part => <Part key={part.id} part={part.name} exercises={part.exercises} /> )}
-    </div>
-  )
-}
-
-const Total = (props) => {
-  const { parts } = props.course
-  const total = parts.reduce((s, p) => s + p.exercises, 0)
-  return (
-    <p><b>total of {total} exercises</b></p>
-  )
-}
-
-const Course = ({course}) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
+import Course from "./components/Course"
 
 const App = () => {
   const courses = [
